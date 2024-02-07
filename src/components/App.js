ce36e4faba5cee4bbe876100c8b3372f56718a45
Dipak1121 <dipaks9604@gmail.com>
2024-02-07 12:53:54 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [showList, setShowList] = useState(false);
+
+  const [apiData, setApiData] = useState([]);
+
+  function fetchAPI(){
+
+    fetch("https://reqres.in/api/users")
+    .then((response)=>response.json())
+    .then((data)=>{
+      console.log(data.data);
+      setApiData(data.data);
+      setShowList(true);
+      console.log(apiData);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+    // console.log(data);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className="head">
+          <h3>Blue Whales</h3>
+          <button onClick={fetchAPI}>Get User List</button>
+          </div>
+          <div className="lebels">
+            <h4>First Name</h4>
+            <h4>Last Name</h4>
+            <h4>Email</h4>
+            <h4>Avatar</h4>
+          </div>
+          {
+            showList && apiData.map((item, index)=>{
+              return(
+                <div className="user" key={index}>
+                  <h4>{item.first_name}</h4>
+                  <h4>{item.last_name}</h4>
+                  <h4>{item.email}</h4>
+                  <img src={item.avatar} alt="No Avatar"></img>
+                </div>
+              )
+            })
+          }
+        
     </div>
   )
 }
