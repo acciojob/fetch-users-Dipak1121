4f8f9134f46186e9502b0133ee231d6d9104b03f
Dipak1121 <dipaks9604@gmail.com>
2024-02-07 14:15:43 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -31,23 +31,47 @@ const App = () => {
           <button className="btn"
           onClick={fetchAPI}>Get User List</button>
           </div>
-          <div className="lebels">
+          {/* <div className="lebels">
             <h4>First Name</h4>
             <h4>Last Name</h4>
             <h4>Email</h4>
             <h4>Avatar</h4>
-          </div>
+          </div> */}
+          <table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              showList && apiData.map((item, index)=>{
+                return(
+                  <tr className="user" key={index}>
+                    <td>{item.first_name}</td>
+                    <td>{item.last_name}</td>
+                    <td>{item.email}</td>
+                    <td><img src={item.avatar} alt="No Avatar"></img></td>
+                  </tr>
+                )
+              })
+            }
+            </tbody>
+          </table>
           {
-            showList && apiData.map((item, index)=>{
-              return(
-                <div className="user" key={index}>
-                  <h4>{item.first_name}</h4>
-                  <h4>{item.last_name}</h4>
-                  <h4>{item.email}</h4>
-                  <img src={item.avatar} alt="No Avatar"></img>
-                </div>
-              )
-            })
+            // showList && apiData.map((item, index)=>{
+            //   return(
+            //     <div className="user" key={index}>
+            //       <h4>{item.first_name}</h4>
+            //       <h4>{item.last_name}</h4>
+            //       <h4>{item.email}</h4>
+            //       <img src={item.avatar} alt="No Avatar"></img>
+            //     </div>
+            //   )
+            // })
           }
         
     </div>
